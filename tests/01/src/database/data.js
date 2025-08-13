97f6c31eb8b6e77f72d950de39b2fd383fffcb0e
André de Paula Terceiro <andredepaula@comerciarios.org.br>
2025-08-13 09:22:06 -0300

    feat: using the username and password from the .env file

--- tests/01/src/database/data.js
+++ tests/01/src/database/data.js
@@ -1,5 +1,5 @@
 async function connectToDatabase(user, password) {
-    if (user == "naruto" && password == "uzumaki") {
+    if (user == process.env.USER_DATABASE && password == process.env.PASSWORD_DATABASE) {
         console.log("Conexão estabelecida");
     } else {
         console.log("erro");
